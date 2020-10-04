cfc2fe95e4600f2079547b10022aa947cd838dc2
mehmeterdemdevge <erdem1devge@gmail.com>
2020-10-04 14:27:11 +0300

    Initial Commit

---
+++ stories/Button.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from '../components/button';
+import * as Icons from '../components/icons';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+export const Emoji = () => {
+  return <Button>save</Button>;
+};
